a5a7ac9017d98c382f1c4f5f6b7cbc4508b08584
Jurica Hladek <jurica.hladek@gmail.com>
2022-11-30 14:35:37 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Loader from './components/Loader';
+import {useEffect, useState} from 'react';
+import { changeProduct, changeProductOption, loadModel } from './connector';
+import Configurator from './components/Configurator';
 
 function App() {
+  const [model, setModel] = useState(undefined);
+  const [selectedProductId, setSelectedProductId] = useState(null);
+  const [selectedOptions, setSelectedOptions] = useState({
+    c1: {
+      o1: "o1-1"
+    }
+  } /*ie. defaults */);
+  
+  useEffect(() => {
+
+    //top level function of useEffect must not be async so I needed a wrapper here
+    async function applyEffect() {
+      const newModel = await loadModel();
+
+      setModel(newModel);
+    }
+
+    applyEffect();
+
+  }, []) //this useEffect handler is not called again unless props.modelName changes
+
+  const handleOptionSelected = (productId, optionId, value) => {
+    setSelectedOptions({
+      ...selectedOptions,
+      [productId]: {
+        ...selectedOptions[productId],
+        [optionId]: value
+      }
+    })
+
+    changeProductOption(productId, optionId, value);
+  }
+
+  const handleProductSelected = productId => {
+    setSelectedProductId(productId);
+    changeProduct(productId);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!!model ? <Configurator model={model} productId={selectedProductId} selectedOptions={selectedOptions} onProductSelected={handleProductSelected} onOptionSelected={handleOptionSelected} /> : <Loader />}
     </div>
   );
 }
